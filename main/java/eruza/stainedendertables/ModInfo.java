7503576d7003922cc136285cfd9e778b58ce55f7
Eruza <askeptical@gmail.com>
2014-07-21 10:50:45 -0400

    Updated version number

--- main/java/eruza/stainedendertables/ModInfo.java
+++ main/java/eruza/stainedendertables/ModInfo.java
@@ -3,7 +3,7 @@ package eruza.stainedendertables;
 public class ModInfo {
 	public static final String MOD_ID = "StainedEnderTables";
 	public static final String MOD_NAME = "Stained Ender Tables";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.01";
 	public static final String GUI_FACTORY = "eruza.stainedendertables.config.SETConfigGuiFactory";
 
 	private ModInfo() {}
