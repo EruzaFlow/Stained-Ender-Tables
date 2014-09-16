08736caa5b5a94bb024bcfdbb5bf16947696d51b
EruzaFlow <askeptical@gmail.com>
2014-09-16 09:33:08 -0400

    Revert "Fixed server crash"

--- main/java/eruza/stainedendertables/network/ClientProxy.java
+++ main/java/eruza/stainedendertables/network/ClientProxy.java
@@ -1,18 +1,3 @@
 package eruza.stainedendertables.network;
 
-import cpw.mods.fml.client.FMLClientHandler;
-import net.minecraft.world.World;
-
-import java.util.Random;
-
-public class ClientProxy extends CommonProxy {
-    @Override
-    public void spawnParticles(double x, double y, double z) {
-        World world = FMLClientHandler.instance().getClient().theWorld;
-        Random rand = new Random();
-        for (int i = 0; i < 32; ++i)
-        {
-            world.spawnParticle("portal", x, y + rand.nextDouble() * 2.0D, z, rand.nextGaussian(), 0.0D, rand.nextGaussian());
-        }
-    }
-}
+public class ClientProxy extends CommonProxy {}
